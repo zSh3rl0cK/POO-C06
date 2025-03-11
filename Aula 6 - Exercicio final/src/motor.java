e2f49a25aeb99fd31571aa01e5726ee519f5b78e
Pedro Henrique Ribeiro Dias <125469206+zSh3rl0cK@users.noreply.github.com>
2025-03-11 13:52:27 -0300

    Delete Aula 6 - Exercicio final directory

--- Aula 6 - Exercicio final/src/motor.java
+++
@@ -1,9 +0,0 @@
-public class motor {
-    int cilindradas;
-    float velocidadeMaxima;
-
-    public void mostrarInfo(){
-        System.out.println("Cilindradas: " + cilindradas );
-        System.out.println("Velocidade: " + velocidadeMaxima  + "\n");
-    }
-}
--- Aula 6 - Exercicio final/src/piloto.java
+++
@@ -1,9 +0,0 @@
-public class piloto {
-    String nome;
-    boolean vilao;
-    kart Kart;
-
-    public void soltaSuperPoder(){
-        System.out.println("O piloto " + nome + " soltou um poder"  + "\n");
-    }
-}
