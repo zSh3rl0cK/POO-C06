3aa8f33b2dffee6a5e0ad5e6011c15a2070a8ae2
Pedro Henrique Ribeiro Dias <125469206+zSh3rl0cK@users.noreply.github.com>
2025-03-11 22:04:04 -0300

    Add files via upload

---
+++ Aula 6/Aula 7/src/Arma.java
@@ -0,0 +1,9 @@
+public class Arma {
+    int dano;
+    int custoDeEnergia;
+
+    public Arma(int dano, int custoDeEnergia) {
+        this.dano = dano;
+        this.custoDeEnergia = custoDeEnergia;
+    }
+}
